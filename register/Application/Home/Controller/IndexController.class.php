b48f0771107555a82effb61ce9f7c1755930c5ec
yanshengsheng <34527846+yanshengsheng@users.noreply.github.com>
2018-01-18 16:35:07 +0800

    登陆

--- register/Application/Home/Controller/IndexController.class.php
+++ register/Application/Home/Controller/IndexController.class.php
@@ -2,14 +2,13 @@
 namespace Home\Controller;
 
 use Think\Controller;
-//use Home\Controller\CommonController;
+use Home\Controller\CommonController;
 class IndexController extends CommonController
 {
 
     public function index()
     {
         echo session('classname');
-//        $this->display('index');
     }
     public function welcome()
     {
