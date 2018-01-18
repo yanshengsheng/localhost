905612a7ba270adeeef5ccd3ccd1b504ccd999f1
yanshengsheng <34527846+yanshengsheng@users.noreply.github.com>
2018-01-18 16:27:36 +0800

    登陆

--- register/Application/Runtime/Data/_fields/yii2.wx_iclass.php
+++
@@ -1,1 +0,0 @@
-a:6:{i:0;s:2:"id";i:1;s:9:"classname";i:2;s:3:"tid";i:3;s:8:"password";s:3:"_pk";s:2:"id";s:5:"_type";a:4:{s:2:"id";s:7:"int(11)";s:9:"classname";s:11:"varchar(10)";s:3:"tid";s:7:"int(11)";s:8:"password";s:12:"varchar(255)";}}
